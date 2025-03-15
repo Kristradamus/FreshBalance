799c1cae1ace227ca0644bc3281422a2f27e4c7a
Kristradamus <115258739+Kristradamus@users.noreply.github.com>
2025-03-15 10:47:16 +0200

    hosting

--- frontend/src/pages/frontPage.jsx
+++ frontend/src/pages/frontPage.jsx
@@ -2,65 +2,76 @@ import { useNavigate} from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import "./frontPage.css";
 
-
 export default function FrontPage() {
- const navigate = useNavigate();
- const { t } = useTranslation();
+const navigate = useNavigate();
+const { t } = useTranslation();
 
 
- const fPWhoWeAreData = {
+const fPTopProductsData = [
+  {icon:"fa-solid fa-truck-fast", text:"Free shipping over 100lv."},
+  {icon:"fa-solid fa-percent", text:"10% off orders over 200lv."},
+  {icon:"fa-solid fa-leaf", text:"100% bio products"},
+];
+const fPWhoWeAreData = [
 
 
- }
-
+];
 
 return (
  <div className="frontPage">
-   <div className="fPHeroSection">
-     <h1 className="fPHeroSectionTitle1">{t("frontPage.heroSectionTitle1")}</h1>
-     <h1 className="fPHeroSectionTitle2">{t("frontPage.heroSectionTitle2")}</h1>
-     <h4 className="fPHeroSectionSubTitle">{t("frontPage.heroSectionSubTitle1")}</h4>
-     <button className="fPHeroSectionLearnMore" onClick={() => {navigate("/about-us")}}>{t("frontPage.heroSectionLearnMore")}<i className="fa-solid fa-location-arrow"></i></button>
-   </div>
-   <hr></hr>
-   <div className="fPTopProducts">
-     <div className="fPTopProductsTitleBox">
-       <h2 className="fPTopProductsTitle1">{t("frontPage.topProductsSectionTitle1")}</h2>
-     </div>
-     <div className="fPRow1">
-
-
-     </div>
-     <div className="fPRow2">
-
-
-     </div>
-     <div className="fPRow3">
-     </div>
-   </div>
-   <div className="fPWhoWeAre">
-   <div className="fPWhoWeAreTitleBox">
-     <h2 className="fPWhoWeAreTitle">WHO WE ARE</h2>
-   </div>
- <div className="fPWhoWeAreBigBox">
- </div>
- <h4 className="fPWhoWeAreSubTitle1">Learn more about our ways from the deeds we have committed</h4>
- <div className="fPWhoWeAreTop"></div>
-</div>
+{/*---------------------------------HERO-SECTION-------------------------------*/}
+    <div className="fPHeroSection">
+      <h1 className="fPHeroSectionTitle1">{t("frontPage.heroSectionTitle1")}</h1>
+      <h1 className="fPHeroSectionTitle2">{t("frontPage.heroSectionTitle2")}</h1>
+      <h4 className="fPHeroSectionSubTitle">{t("frontPage.heroSectionSubTitle1")}</h4>
+      <button className="fPHeroSectionLearnMore" onClick={() => {navigate("/about-us")}}>{t("frontPage.heroSectionLearnMore")}<i className="fa-solid fa-location-arrow"></i></button>
+    </div>
+{/*--------------------------------TOP-PRODUCTS--------------------------------*/}
+    <div className="fPTopProducts">
+      <div className="fPTopProductsTitleBox">
+        <h2 className="fPTopProductsTitle1">{t("frontPage.topProductsSectionTitle1")}</h2>
+      </div>
+      <div className="fPTopProductsBenefits">
+      {fPTopProductsData.map((item, index) => {
+        <ul>
+          <li key={index}>
+            <div className="fPTopProductBenefit">
+              <img src={item.icon}></img><p>{item.text}</p>
+            </div>
+          </li>
+        </ul>
+        })}
+      </div>
+      <div className="fPLongRow">
+      </div>
+      <div className="fPMiddleRow">
+      </div>
+      <div className="fPLongRow">
+      </div>
+    </div>
+{/*---------------------------------------WHO-WE-ARE--------------------------------*/}
+    <div className="fPWhoWeAre">
+      <div className="fPWhoWeAreTitleBox">
+        <h2 className="fPWhoWeAreTitle">WHO WE ARE</h2>
+      </div>
+      <div className="fPWhoWeAreBigBox">
+      </div>
+      <h4 className="fPWhoWeAreSubTitle1">Learn more about our ways from the deeds we have committed</h4>
+      <div className="fPWhoWeAreTop"></div>
+    </div>
    <div className="fPWhoWeWorkWith">
-     <div>
-       <div className="fPWhoWeWorkWithTitle1">
-         <h1>Here are some of the brands we work with</h1>
-         <h4></h4>
-       </div>
-     </div>
-   </div>
-   <div className="fPReviews">
-     <div className="fPReviewsTitleBox">
-       <h1>Don't just take our word, see what other clients think about us</h1>
-     </div>
-    
-   </div>
- </div>
- )
-}
+    <div>
+      <div className="fPWhoWeWorkWithTitle1">
+        <h1>Here are some of the brands we work with</h1>
+        <h4></h4>
+      </div>
+    </div>
+  </div>
+  <div className="fPReviews">
+    <div className="fPReviewsTitleBox">
+      <h1>Don't just take our word, see what other clients think about us</h1>
+    </div>
+  
+  </div>
+</div>
+);};
