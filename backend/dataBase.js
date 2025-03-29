600254ac93b3f8e7c5b83eb9a34285bc917aec14
Kristradamus <115258739+Kristradamus@users.noreply.github.com>
2025-03-29 19:44:12 +0200

    Database

--- backend/dataBase.js
+++ backend/dataBase.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "nutrition_website2.0",
+    database: process.env.DB_NAME || "fresh_balance",
     connectionLimit: 10,
 })
 
