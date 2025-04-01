76081b5dce2de905ffc7ee4902127d99199a486d
Kristradamus <115258739+Kristradamus@users.noreply.github.com>
2025-04-01 18:27:33 +0300

    SmallChanges

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,19 +1,41 @@
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { AuthContext } from './AuthContext.jsx';
-import LoadingAnimation from "./layout/LoadingAnimation.jsx"
+import { useContext, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { AuthContext } from "./AuthContext.jsx";
+import LoadingAnimation from "./layout/LoadingAnimation.jsx";
+import { useTranslation } from "react-i18next";
+import "./ProtectedRoute.css";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useContext(AuthContext)
+const { isAuthenticated, isLoading } = useContext(AuthContext);
+const { t } = useTranslation();
+const navigate = useNavigate();
+const location = useLocation();
 
-  if (isLoading) {
-    return <LoadingAnimation />;
+useEffect(() => {
+  if (!isLoading && !isAuthenticated) {
   }
+}, [isAuthenticated, isLoading]);
 
-  if (!isAuthenticated) {
-    return <Navigate to="/email-check" replace />;
-  }
-  return children;
+if (isLoading) {
+  return <LoadingAnimation />;
+}
+
+if (!isAuthenticated) { return (
+  <div className="authAlertContainer">
+    <div className="authAlertBox">
+      <div className="authAlertText">
+        <h2 className="authAlertMainTitle">{t("protectedRoute.loginRequiredTitle")}</h2>
+        <p className="authAlertSubTitle">{t("protectedRoute.loginRequiredSubTitle")}</p>
+      </div>
+      <div className="authAlertButtons">
+        <button className="authAlertBtn" onClick={() => navigate('/email-check', {state: { from: location.pathname }})}>{t("protectedRoute.login")}</button>
+        <button className="authAlertCancelBtn" onClick={() => {navigate(-2);}}>{t("protectedRoute.cancel")}</button>
+      </div>
+    </div>
+  </div>
+);};
+
+return children;
 };
 
 export default ProtectedRoute;
